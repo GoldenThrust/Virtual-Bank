7ddd32eedd82b7cd4b66e84a9a27bd80282749e1
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-11-17 02:52:05 +0100

    Users model updated

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,11 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_birth = models.DateField(null=True)
     profile_picture = models.ImageField(default="profile.png")
-    phone_number = models.IntegerField(null=True, blank=True)
+    phone_number = models.IntegerField(null=True)
     address = models.CharField(max_length=255, null=True)
     city = models.CharField(max_length=100, null=True)
     state = models.CharField(max_length=100, null=True)
     country = models.CharField(max_length=50, null=True)
     ip_address = models.GenericIPAddressField(unpack_ipv4=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.username:
+            self.username = f"users_{User.objects.count() + 1}"
+        super().save(*args, **kwargs)
