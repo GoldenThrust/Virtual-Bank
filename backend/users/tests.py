64dc71e35f8d1414f9cea596bc073388055504b6
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-11-21 23:45:01 +0100

    accounts and creditcard api for users

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,18 +1,3 @@
 from django.test import TestCase
 
 # Create your tests here.
-class UserTest(TestCase):
-    sum =     {
-        "username": "mike_smith",
-        "first_name": "Mike",
-        "last_name": "Smith",
-        "password": "mikesmith",
-        "email": "m.smith@example.com",
-        "address": "789 Oak Ave",
-        "city": "Sunnydale",
-        "state": "TX",
-        "country": "USA",
-        "date_of_birth": "1988-12-05",
-        "phone_number": 112233445566,
-        "ip_address": "10.0.0.1"
-    }
