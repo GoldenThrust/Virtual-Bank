0aeb55d03a7e735e7183f7268905656072c39e34
Adeniji Olajide <adenijiolajid01.com>
2023-12-10 11:51:34 +0100

    Update README.md

--- rootinstall.sh
+++ rootinstall.sh
@@ -6,6 +6,11 @@ wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-
 sudo apt-get update
 sudo apt-get -y install postgresql
 
+# Clone the repository
+git clone https://github.com/GoldenThrust/Virtual-Bank.git
+
+cd Virtual-Bank
+
 # install Python virtual environmen
 sudo apt install python3.8-venv
 
