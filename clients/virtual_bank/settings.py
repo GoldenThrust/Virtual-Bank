77191c937c919270a71e3e0b265d4b47b44f6858
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-12-14 15:37:53 +0100

    No Commit Message

--- clients/virtual_bank/settings.py
+++ clients/virtual_bank/settings.py
@@ -190,3 +190,10 @@ LOGOUT_REDIRECT_URL = "home:homepage"
 LOGIN_REDIRECT_URL = 'users:dashboard'
 
 LOGIN_URL = 'users:login'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
