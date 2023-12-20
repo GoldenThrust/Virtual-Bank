f88125da04133d06237806f32c4e65bae193ac75
Adeniji Olajide <adenijiolajid01@gmail.com>
2023-12-20 17:35:49 +0100

    Notification

--- clients/notifications/templatetags/notifications_tags.py
+++ clients/notifications/templatetags/notifications_tags.py
@@ -10,3 +10,12 @@ def unread_notifications_count(context):
     if user.is_authenticated:
         return Notification.objects.select_related('user').filter(user=user, status='UNREAD').count()
     return 0
+
+@register.simple_tag(takes_context=True)
+def unread_notification(context):
+    from notifications.models import Notification
+
+    user = context['request'].user
+    if user.is_authenticated:
+        return Notification.objects.select_related('user').filter(user=user, status='UNREAD').order_by('-created_date')[:50]
+    return 0
---
+++ clients/virtual_bank/utils.py
@@ -0,0 +1,43 @@
+from django.utils import timezone
+
+def calculate_time_difference_from_now(past_time):
+    # Convert the past_time string to a timezone-aware datetime object
+    past_datetime = timezone.datetime.strptime(past_time, '%Y-%m-%d %H:%M:%S')
+    past_datetime = timezone.make_aware(past_datetime)
+    
+    # Get the current time in the timezone
+    current_datetime = timezone.now()
+    
+    # Calculate the time difference
+    time_difference = current_datetime - past_datetime
+    
+    # Convert time difference to seconds
+    total_seconds = time_difference.total_seconds()
+    
+    # Calculate years, months, weeks, days, hours, and minutes
+    years = total_seconds // (365 * 24 * 3600)
+    months = total_seconds // (30 * 24 * 3600)
+    weeks = total_seconds // (7 * 24 * 3600)
+    days = total_seconds // (24 * 3600)
+    hours = total_seconds // 3600
+    minutes = total_seconds // 60
+    
+    # Generate human-readable time difference string
+    if years > 0:
+        return f"{int(years)} {'year' if years == 1 else 'years'} ago"
+    elif months > 0:
+        return f"{int(months)} {'month' if months == 1 else 'months'} ago"
+    elif weeks > 0:
+        return f"{int(weeks)} {'week' if weeks == 1 else 'weeks'} ago"
+    elif days > 0:
+        return f"{int(days)} {'day' if days == 1 else 'days'} ago"
+    elif hours > 0:
+        return f"{int(hours)} {'hour' if hours == 1 else 'hours'} ago"
+    elif minutes > 0:
+        return f"{int(minutes)} {'minute' if minutes == 1 else 'minutes'} ago"
+    else:
+        return 'just now'
+
+past_time = '2023-12-18 18:30:00'
+time_ago = calculate_time_difference_from_now(past_time)
+print(time_ago)
