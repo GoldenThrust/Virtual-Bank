7231aae5ff3f15623901a690c02ee8f11b50e7be
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-08-30 15:24:52 +0100

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
-sudo apt-get upgrade
+# Update package manager (without sudo)
+apt-get update
+apt-get upgrade -y
 
-# install Python virtual environmen
-sudo apt install python3.10-venv
+# Install Python environment tools (without sudo)
+apt-get install -y python3.10-venv
 
-sudo apt-get install build-essential
-sudo apt-get install python3-dev
+# Install build essentials and Python development headers
+apt-get install -y build-essential python3-dev
 
+# Install pip (you don't need sudo here either)
+apt-get install -y python3-pip
 
-# install pip
-sudo apt install python3-pip
-
-# Upgrade pip and install requirements within the virtual environment
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+# Upgrade pip and install requirements
+pip3 install --upgrade pip
+pip3 install -r requirements.txt
