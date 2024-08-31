efde8c027a6165a326d1f70ee6f257b8bc6eb48c
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-08-31 21:35:38 +0100

    No Commit Message

--- api/api/views.py
+++ api/api/views.py
@@ -3,7 +3,6 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .serializers import URLSerializer
 
-
 class ListApiUrls(APIView):
     permission_classes = []
     authentication_classes = [] 
--- api/virtual_bank/urls.py
+++ api/virtual_bank/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-if settings.DEBUG is False:
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
