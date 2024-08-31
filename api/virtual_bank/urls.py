1d85da427966cfc3796396d1e9150e8dbddfbdc0
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-08-31 21:40:43 +0100

    No Commit Message

--- api/virtual_bank/urls.py
+++ api/virtual_bank/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('', include('api.urls'), name='api'),
     path('admin/', admin.site.urls),
